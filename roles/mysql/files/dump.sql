30815d38028b1534a8c8e786e28de520f6fa0a98
davideCompagnone <dvd.cmp.1991@gmail.com>
2023-09-02 16:39:12 +0200

    correzione esercizio2

--- roles/mysql/files/dump.sql
+++ roles/mysql/files/dump.sql
@@ -1,12 +1,23 @@
 use users;
 
 CREATE TABLE IF NOT EXISTS users (
-  id varchar(255) NOT NULL,
+  id varchar(255) NOT NULL PRIMARY KEY,
   username varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
+  email varchar(255) NOT NULL
 );
 
-INSERT INTO users (id, username, email) VALUES('1', 'martina', 'martinalil@hotmail.it');
-INSERT INTO users (id, username, email) VALUES('2', 'giuliano', 'giusol@hotmail.it');
-INSERT INTO users (id, username, email) VALUES('3', 'edgar', 'edpol@hotmail.it');
-INSERT INTO users (id, username, email) VALUES('4', 'michele', 'micpuz@hotmail.it');
+
+INSERT IGNORE INTO users (id, username, email) VALUES('1', 'martina', 'martinalil@hotmail.it');
+
+   INSERT IGNORE INTO users (id, username, email) VALUES('2', 'giuliano', 'giusol@hotmail.it');
+
+   INSERT IGNORE INTO users (id, username, email) VALUES('3', 'edgar', 'edpol@hotmail.it');
+
+   INSERT IGNORE INTO users (id, username, email) VALUES('4', 'michele', 'micpuz@hotmail.it');
+
+   INSERT IGNORE INTO users (id, username, email) VALUES('5', 'davide', 'davcomp@hotmail.it');
+
+
+
+
+
